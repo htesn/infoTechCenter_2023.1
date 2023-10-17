8c97f05a2af9a16e4b399391bd210988612fdb2c
seth <seth.norris6890@gmail.com>
2023-10-17 08:30:46 -0400

    Add description and Welcome Print lines

---
+++ ProjectOrangutan.py
@@ -0,0 +1,8 @@
+"""
+Our Welcome Screen will start our program letting
+drivers know that InfoTech Center 2023 is loading
+"""
+
+print("\n\nWelcome - InfoTech Center 2023")
+
+print("\nInfoTech Center 2023 is loading...")
