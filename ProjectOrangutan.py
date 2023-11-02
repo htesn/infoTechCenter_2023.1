9e4972ec88761100ff1aad6408e11f10ea320507
seth <seth.norris6890@gmail.com>
2023-11-02 08:10:59 -0400

    Add gasLevelAlert function w/ if, elif, else statements

--- ProjectOrangutan.py
+++ ProjectOrangutan.py
@@ -16,9 +16,34 @@ def listOfGasStation():
     gasStations =["Shell","Speedway","Exxon","Meijers","Costco","Marathon","BP","Circle K","Wesco"]
     gasStationsNearby = random.choice(gasStations)
     return gasStationsNearby
-    
 
 # Function will call the gasLevelGauge to determine gas level and then find a close gas station if low
-def gasLevelAlret():
+def gasLevelAlert():
     milesToGasStationsLow = round(random.uniform(1,25),1)
     milesToGasStationsQaurterTank = round(random.uniform(25.1,50),1)
+    gasLevelIndicator = gasLevelGauge()
+    if gasLevelIndicator == "Empty":
+        print("***WARNING - YOU ARE ON EMPTY***")
+        sleep(1.25)
+        print("Calling Triple AAA")
+    elif gasLevelIndicator == "Low":
+        print("Your gas tank is extremely low,checking Google Maps for the closest gas station.")
+        sleep(1.5)
+        print("The Closest gas station is",listOfGasStation(),"which is",milesToGasStationsLow, "miles away.")
+    elif gasLevelIndicator == "Quarter Tank":
+        print("You have a Quarter of a Tank of gasoline,checking Google Maps for the closest gas station.")
+        sleep(1.5)
+        print("The Closest gas station is", listOfGasStation(), "which is", milesToGasStationsLow, "miles away.")
+    elif gasLevelIndicator == "Half Tank":
+        print("Your gas tank has a half of a tank which is enough to get to your destinations.")
+    elif gasLevelIndicator == "Three quarter Tank":
+        print("Your gas tank is at three quarter tank!")
+    else:
+        print("Your gas tank is full -Yeah! - Congratulation! -Vroom Vroom!")
+
+
+gasLevelAlert()
+
+
+
+
